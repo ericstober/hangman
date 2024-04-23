42ecd0ecb5e5a36006867ab199547a2dc856d892
ericstober <erictstober@gmail.com>
2024-04-23 15:32:21 -0500

    added displayWords function:

---
+++ script.js
@@ -0,0 +1,39 @@
+const wordElement = document.getElementById('word');
+const wrongLettersElement = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-again');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// Show the hidden word
+function displayWord() {
+  wordElement.innerHTML = `
+    ${selectedWord
+      .split('')
+      .map (letter => `
+        <span class="letter">
+          ${correctLetters.includes(letter) ? letter : ''}
+        </span>
+      `)
+      .join('')
+    }
+  `;
+
+  const innerWord = wordElement.innerText.replace(/\n/g, '');
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = 'Congratulations you won!';
+    popup.style.display = 'flex';
+  }
+}
+
+displayWord();
